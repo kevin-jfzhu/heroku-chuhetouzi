e8e5308625fe489118c3e18141e2248d7923f31c
kevinzhu@macos <kevin_jfzhu@163.com>
2019-07-16 01:33:28 +0800

    update contents for chuhetouzi

--- root/app.py
+++ root/app.py
@@ -5,22 +5,17 @@ from sqlalchemy import create_engine
 
 
 app = Flask(__name__)
-engine = create_engine('sqlite:///data/data_pro.db')
+# engine = create_engine('sqlite:///data/data_pro.db')
 
 
 @app.route('/', methods=['GET'])
 @app.route('/index', methods=['GET'])
 def index():
-    # df = pd.read_sql_table('chuheyihao_performance', con=engine, schema='main')
-    # df1 = df[['unit_value', 'unit_value_change']]
-    # df1.index = df.date
     return render_template('index.html')
 
 
 @app.route('/performance/chuheyihao', methods=['GET'])
 def product_performance():
-    df = pd.read_sql_table('chuheyihao_performance', con=engine, schema='main')
-    df1 = df[['unit_value', 'unit_value_change']]
-    df1.index = df.date
-    return jsonify(json.loads(df1.to_json()))
+    df1 = {'a': 5, 'b': 7, 'c': 9}
+    return jsonify(df1)
 
